0c6d369af4b891c10d93b2a3cae820cdb54e7ff7
misha <mishazlupko@gmail.com>
2023-08-30 17:04:35 +0300

    feature: finish card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 import s from "../components/CardBikeComponent.module.scss";
-import CardBikeComponent from "../components/CardBikeComponent";
 
 const Home = () => {
   return (
     <div className={s.container}>
       <h1>Home</h1>
-      <CardBikeComponent id={1} />
-      <CardBikeComponent id={2} />
     </div>
   );
 };
--- components/CardBikeComponent.tsx
+++ components/CardBikeComponent.tsx
@@ -1,11 +1,13 @@
+"use client";
+
 import s from "./CardBikeComponent.module.scss";
 
 import React from "react";
-import Image from "next/image";
+import { useState } from "react";
 import { PiHeartBold } from "react-icons/pi";
 import { PiShoppingCartSimpleBold } from "react-icons/pi";
-import { TbTrash } from "react-icons/tb";
 import Link from "next/link";
+import Image from "next/image";
 import data from "../data/cards-bike.json";
 
 interface CardBikeProps {
@@ -19,21 +21,30 @@ interface CardBikeProps {
 }
 
 const CardBikeComponent: React.FC<{ id: number }> = ({ id }) => {
-  const cards: { [key: string]: CardBikeProps } = data;
+  const initialCards: { [key: string]: CardBikeProps } = data;
+  const [cards, setCards] = useState(initialCards);
 
   const infoCard = cards[id];
   const category = infoCard.category;
 
+  const clickOnCart = () => {
+    setCards({
+      ...cards,
+      [id]: { ...infoCard, basket: !infoCard.basket },
+    });
+  };
+
+  const clickOnLike = () => {
+    setCards({
+      ...cards,
+      [id]: { ...infoCard, like: !infoCard.like },
+    });
+  };
+
   return (
     <div className={s.card}>
       <Link href={`/catalog/${category}/${id}`}>
-        <Image
-          src={infoCard.image}
-          alt="Bike"
-          width={295}
-          height={300}
-          className={s.card__photo}
-        />
+        <Image src={infoCard.image} alt="Bike" width={390} height={314} />
       </Link>
       <div className={s.card__wrapper}>
         <div className={s.card__wrapper_color}>
@@ -45,15 +56,16 @@ const CardBikeComponent: React.FC<{ id: number }> = ({ id }) => {
         </Link>
         <p className={s.card__price}>{infoCard.price}</p>
         <div className={s.card__wrapper_icon}>
-          {!infoCard.like && <PiHeartBold className={s.card__icon_heard} />}
-          {/* <div className={s.card__icon}> */}
-          <PiShoppingCartSimpleBold className={s.card__icon_cart} />
-          {/* <p>Add to cart</p> */}
-          {/* </div> */}
-          {infoCard.like && <TbTrash />}
+          <div onClick={clickOnLike} className={s.card__wrapper_heard}>
+            <PiHeartBold className={s.card__icon_heard} />
+          </div>
+          <div className={s.card__wrapper_cart} onClick={clickOnCart}>
+            <PiShoppingCartSimpleBold className={s.card__icon_cart} />
+            <p className={s.card__text_cart}>Add to cart</p>
+          </div>
         </div>
       </div>
-      {infoCard.discount !== "" && (
+      {infoCard.discount && (
         <div className={s.card__discount}>
           <p className={s.card__discount_text}>{infoCard.discount}</p>
         </div>
