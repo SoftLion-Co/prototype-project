fc979231e599294a63307551f22098fe65d5e8eb
FeMaxxx <119453050+FeMaxxx@users.noreply.github.com>
2023-08-31 14:58:34 +0300

    refactor: add link to category, fix title, text and breadcrumbs styles

--- sections/catalog_page/CatalogSection.tsx
+++ sections/catalog_page/CatalogSection.tsx
@@ -3,33 +3,38 @@ import BreadcrumbsComponent from "@/components/BreadcrumbsComponent";
 import s from "./CatalogSection.module.scss";
 import categories from "../../data/categories.json";
 import Image from "next/image";
+import Link from "next/link";
 
 export const CatalogSection: FC = () => {
   const links = [{ title: "Catalog", href: "" }];
 
   return (
     <div className={s.catalog_section__bg}>
-      <BreadcrumbsComponent links={links} />
-
       <section className={s.container}>
+        <div className={s.catalog_section__breadcrumbs_container}>
+          <BreadcrumbsComponent links={links} />
+        </div>
+
         <div className={s.catalog_section}>
           <h1 className={s.catalog_section__title}>Catalog specialized</h1>
 
           <ul className={s.catalog_section__list}>
             {Object.entries(categories).map(([key, value]) => {
               return (
-                <li key={key} className={s.catalog_section__item}>
-                  <Image
-                    className={s.catalog_section__item__image}
-                    src={value}
-                    alt="bike"
-                    width={200}
-                    height={100}
-                  />
+                <Link className={s.catalog_section__link} href={`/catalog/${key}`}>
+                  <li key={key} className={s.catalog_section__item}>
+                    <Image
+                      className={s.catalog_section__item__image}
+                      src={value}
+                      alt="bike"
+                      width={200}
+                      height={100}
+                    />
 
-                  <h4 className={s.catalog_section__item__title}>{key}</h4>
-                  <p className={s.catalog_section__item__text}>55 in stock</p>
-                </li>
+                    <h4 className={s.catalog_section__item__title}>{key}</h4>
+                    <p className={s.catalog_section__item__text}>55 in stock</p>
+                  </li>
+                </Link>
               );
             })}
           </ul>
