8f5884ee3e1fa2ee5561b772fd196962776b74a9
zhuvakAnd <125382431+zhuvakAnd@users.noreply.github.com>
2023-08-31 16:24:15 +0300

    refactor:item page

--- app/catalog/[caregory]/[id]/page.tsx
+++ app/catalog/[caregory]/[id]/page.tsx
@@ -1,9 +1,24 @@
 "use client";
 
 import { usePathname, redirect } from "next/navigation";
+import data from "@/data/cards-bike.json";
 
 const Category = () => {
-  const id = usePathname().split("/").reverse()[0];
+  const items: { [key: string]: { category: string } } = data;
+  const path = usePathname().split("/").reverse();
+  const id = path[0];
+  const category = path[1];
+  if (!Object.keys(data).includes(id)) {
+    redirect(
+      `/catalog/${items[Object.keys(items)[0]].category}/${
+        Object.keys(items)[0]
+      }`
+    );
+  }
+
+  if (items[id].category !== category) {
+    redirect(`/catalog/${items[id].category}/${id}`);
+  }
 
   return (
     <div>
