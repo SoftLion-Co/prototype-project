d9e0995232d1887514339fdbb97285ca1276f526
zhuvakAnd <125382431+zhuvakAnd@users.noreply.github.com>
2023-09-11 15:40:04 +0300

    Update page.tsx

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,6 +1,8 @@
 import OrderSection from "@/sections/order_section/OrderSection";
 
 const Cart = () => {
+  const links = [{ title: "Cart", href: "" }];
+
   return (
     <div>
       <OrderSection />
