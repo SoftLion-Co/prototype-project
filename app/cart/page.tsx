c906c6b54d56611e5fc5e4c6a16ac8028725044c
zhuvakAnd <125382431+zhuvakAnd@users.noreply.github.com>
2023-09-13 11:41:41 +0300

    code clean up

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,48 +1,14 @@
 "use client";
 
-import Breadcrumbs from "@/components/BreadcrumbsComponent";
+import BreadcrumbsComponrnt from "@/components/BreadcrumbsComponent";
 import CartMain from "@/components/cartpage/CartMainComponent";
-import DeliverySection from "@/sections/delivery_page/DeliverySection";
-import useCartlist from "@/hooks/useCartlist";
-
 const Cart = () => {
   const links = [{ title: "Cart", href: "" }];
-  const { cartItems, setItems } = useCartlist();
 
   return (
     <div>
-      <p>{JSON.stringify(cartItems)}</p>
-      <button
-        onClick={() => {
-          setItems("1", 5);
-        }}
-      >
-        1
-      </button>
-      <button
-        onClick={() => {
-          setItems("2", 2);
-        }}
-      >
-        2
-      </button>
-      <button
-        onClick={() => {
-          setItems("3", 3);
-        }}
-      >
-        3
-      </button>
-      <button
-        onClick={() => {
-          setItems("1", 0);
-        }}
-      >
-        1
-      </button>
       <Breadcrumbs links={links} />
       <CartMain />
-      <DeliverySection />
     </div>
   );
 };
