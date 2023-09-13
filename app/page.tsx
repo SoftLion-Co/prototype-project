c9d8055811c3a5aaf98de6e99b93dc7f7a566d0e
zhuvakAnd <125382431+zhuvakAnd@users.noreply.github.com>
2023-09-13 14:03:37 +0300

    json update

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,13 @@ import s from "../components/CardBikeComponent.module.scss";
 import { Dealer } from "@/sections/home_page/Dealer";
 import SliderComponent from "@/components/SliderComponent";
 import { HeroSection } from "@/sections/home_page/HeroSection";
+import data from "../data/cards-bike.json";
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
-      <SliderComponent id={["1", "2", "3", "4", "5", "6", "7"]} />
+      <SliderComponent id={Object.keys(data)} />
       <Dealer />
     </div>
   );
